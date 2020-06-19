2506f94bdc5ee4c24db989eb48e928406ac5f562
Kurt Krafft <kkrafft6609@gmail.com>
2020-06-19 14:43:48 -0500

    added account page

---
+++ src/components/account/account.js
@@ -0,0 +1,67 @@
+import React, {useState, useEffect} from 'react'
+import UM from "../../modules/userManager"
+import "./account.css"
+
+const AccountPage = props => {
+    const [customer, setCustomer] = useState({})
+    const [isLoading, setIsLoading] = useState(true)
+    const token = sessionStorage.getItem('token')
+
+
+
+
+
+    useEffect(()=> {
+        UM.getUserInformation(token).then(obj=> setCustomer(obj)).then(()=>setIsLoading(false))
+    },[])
+
+    if(isLoading){
+        return (
+            <>
+            <div className="loader-container"><div className="loader"></div></div>
+            </>
+        )
+    }
+    else {
+
+        return (
+            <>
+            <div className="account-page">
+            <div className="profile-holder">
+                <div className="name-n-img">
+                        <div className="img-thumbnail"><img src="https://pecb.com/conferences/wp-content/uploads/2017/10/no-profile-picture.jpg" alt="prof-pic" className="prof-pic-icon"/></div>
+                        <div className="name-holder push-down"><h1>{customer.user.first_name} {customer.user.last_name}</h1></div>
+                </div>
+                <div className="customer-info">
+                    <h1 className="information-title">Information</h1>
+                    <div className="detail-holder">
+                        <div className="email-holder">
+                            <h2 className="account-title">Email</h2>
+                            <h5 className="account-detail">{customer.user.email}</h5>
+                        </div>
+                        <div className="address-holder">
+                            <h2 className="account-title">Address</h2>
+                            <h5 className="account-detail">{customer.address}</h5>
+                        </div>
+                    </div>
+                    <div className="detail-holder add-marg-top">
+                        <div className="email-holder">
+                            <h2 className="account-title">City</h2>
+                            <h5 className="account-detail">{customer.city}</h5>
+                        </div>
+                        <div className="address-holder">
+                            <h2 className="account-title">Phone Number</h2>
+                            <h5 className="account-detail">{customer.phone_number}</h5>
+                        </div>
+                    </div>
+                    <div className="edit-icon-account">
+                        <i class="edit icon" onClick={()=>props.history.push('/account/edit')}></i>
+                    </div>
+                </div>
+            </div>
+            </div>
+            </>
+        )
+    }
+}
+export default AccountPage
---
+++ src/components/account/accountEdit.js
@@ -0,0 +1,116 @@
+import React, {useState, useEffect} from 'react'
+import UM from "../../modules/userManager"
+import "./accountEdit.css"
+import {Button} from "semantic-ui-react"
+
+const EditAccount = props => {
+    const [customer, setCustomer] = useState({})
+    const [userEmail, setUserEmail] = useState({"email": ""})
+    const [isLoading, setIsLoading] = useState(true)
+    const token = sessionStorage.getItem('token')
+
+    const handleFieldChange = e=> {
+        const stateToChange = {...customer}
+        if(e.target.id==="email"){
+            stateToChange.user.email = e.target.value
+        }else {
+            stateToChange[e.target.id] = e.target.value
+        }
+        
+        setCustomer(stateToChange)
+    }
+
+    const handleEmailChange = e=> {
+        const stateToChange = {...userEmail}
+        stateToChange[e.target.id] = e.target.value
+        setUserEmail(stateToChange)
+    }
+    const handleSubmit = e=> {
+        e.preventDefault()
+        const updated_customer = {
+            "id": customer.id,
+            "address": customer.address,
+            'city': customer.city,
+            "phone_number": customer.phone_number
+        }
+        const updated_user = {
+            "id": customer.user.id,
+            "email": customer.user.email
+        }
+        UM.updateCustomer(token, updated_customer).then(()=> {
+            UM.updateUser(token, updated_user).then(()=> props.history.push('/account'))
+        })
+
+    }
+
+
+
+    useEffect(()=> {
+        UM.getUserInformation(token).then(obj=> {
+            setUserEmail(obj.user.email)
+            setCustomer(obj)
+        }).then(()=>{setIsLoading(false)})
+    },[])
+
+    if(isLoading){
+        return (
+            <>
+            <div className="loader-container"><div className="loader"></div></div>
+            </>
+        )
+    }
+    else {
+
+        return (
+            <>
+            <div className="account-page">
+            <div className="profile-holder">
+                <div className="name-n-img">
+                        <div className="img-thumbnail"><img src="https://pecb.com/conferences/wp-content/uploads/2017/10/no-profile-picture.jpg" alt="prof-pic" className="prof-pic-icon"/></div>
+                        <div className="name-holder push-down"><h1>{customer.user.first_name} {customer.user.last_name}</h1></div>
+                </div>
+                <div className="customer-info">
+                    <h1 className="information-title">Information</h1>
+                    <div class="page add-marg-left">
+                    <div className="fields">
+                    <label class="field a-field a-field_a1">
+                        <input class="field__input a-field__input" id="email" onChange={handleFieldChange} value={customer.user.email} />
+                        <span class="a-field__label-wrap">
+                        <span class="a-field__label">Email</span>
+                        </span>
+                    </label>
+                    <label class="field a-field a-field_a1">
+                        <input class="field__input a-field__input" id="city"value={customer.city} onChange={handleFieldChange}  />
+                        <span class="a-field__label-wrap">
+                        <span class="a-field__label">City</span>
+                        </span>
+                    </label>
+                     
+                    </div>
+                    <div className="fields add-top-to-fields">
+                    <label class="field a-field a-field_a2">
+                        <input class="field__input a-field__input" id="address"value={customer.address} onChange={handleFieldChange} />
+                        <span class="a-field__label-wrap">
+                        <span class="a-field__label">Address</span>
+                        </span>
+                    </label>   
+                    
+                    <label class="field a-field a-field_a2">
+                        <input class="field__input a-field__input" id="phone_number" value={customer.phone_number} onChange={handleFieldChange}  />
+                        <span class="a-field__label-wrap">
+                        <span class="a-field__label">Phone Number</span>
+                        </span>
+                    </label>  
+                    </div>
+                    </div>
+                </div>
+                <div className="edit-icon-account">
+                    <Button disabled={isLoading} onClick={handleSubmit}>Submit</Button>
+                </div>
+            </div>
+            </div>
+            </>
+        )
+    }
+}
+export default EditAccount
